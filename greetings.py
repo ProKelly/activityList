26d2e7b72a8f08eb1145737611faa72f903c5add
Prince Kelly <121863540+ProKelly@users.noreply.github.com>
2024-02-27 17:03:29 -0800

    Created greetings.py

---
+++ greetings.py
@@ -0,0 +1,5 @@
+print('''Hello dear interns, this project will help you, understand how to perform various Create, Read, Updata and Delete (CRUD) operations,
+        in relational database when building websites with python django''')
+
+print()
+print("by Engr Prince Kelly")
